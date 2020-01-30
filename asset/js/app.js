b2491f09b541129683d4bc2dceca2e0086f6957e
DroidTamvan <ragilsuryapratama1@gmail.com>
2020-01-30 14:18:40 +0700

    Update

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/ServiceWorker.js').then(function(registration) {
+    navigator.serviceWorker.register('asset/js/ServiceWorker.js').then(function (registration) {
       console.log('ServiceWorker registration successful with scope');
     }, function(err) {
       console.log('ServiceWorker registration failed: ', err);
