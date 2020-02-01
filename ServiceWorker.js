2e3ecd3adc210946abd5a1d67c827c5a7f39f89c
DroidTamvan <ragilsuryapratama@gmail.com>
2020-02-01 22:23:51 +0700

    Update

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,16 +1,16 @@
-var cacheNames = 'cache-as';
+var cacheNames = 'cache-asw';
 var fileCache = [
   './favicon.ico',
   'https://2.bp.blogspot.com/-eeT0PbyC26U/Xcg-l9eXVxI/AAAAAAAAAe0/xRbp8w_-x_o4phNWS8YnqAMS34tXfMukwCLcBGAsYHQ/s1600/1-min.jpg',
   'https://2.bp.blogspot.com/-pkBo4euuNjE/Xcg-ljg1BaI/AAAAAAAAAew/fkErSFYsUiMnUE7UL8rlHTR3orpL65SXQCLcBGAsYHQ/s1600/2-min.jpg',
   'https://1.bp.blogspot.com/-_2IL0vYZARQ/Xcg-lvLBawI/AAAAAAAAAes/9RuuIcFV7gEzf5eIafB3rMK79tt3hsWVgCLcBGAsYHQ/s1600/3-min.jpg',
-  'https://cdn.jsdelivr.net/gh/DroidTamvan/css-js@2.3/donation.png',
-  'https://cdn.jsdelivr.net/gh/DroidTamvan/css-js@2.3/DT-XrX.webp',
-  'https://cdn.jsdelivr.net/gh/DroidTamvan/css-js@2.3/thumbnail-yt.webp',
-  'https://cdn.jsdelivr.net/gh/DroidTamvan/css-js@2.3/SAMPM.jpg',
-  'https://cdn.jsdelivr.net/gh/DroidTamvan/css-js@2.3/jquery.js',
-  'https://cdn.jsdelivr.net/gh/DroidTamvan/css-js@2.3/bootstrap.css',
-  'https://cdn.jsdelivr.net/gh/DroidTamvan/css-js@2.3/bootstrap.js'
+  'https://cdn.jsdelivr.net/gh/DroidTamvan/droidtamvan.github.io/src/img/donation.png',
+  'https://cdn.jsdelivr.net/gh/DroidTamvan/droidtamvan.github.io/src/img/DT-XrX.webp',
+  'https://cdn.jsdelivr.net/gh/DroidTamvan/droidtamvan.github.io/src/img/thumbnail-yt.webp',
+  'https://cdn.jsdelivr.net/gh/DroidTamvan/droidtamvan.github.io/src/img/SAMPM.jpg',
+  'https://cdn.jsdelivr.net/gh/DroidTamvan/droidtamvan.github.io/src/js/jquery.js',
+  'https://cdn.jsdelivr.net/gh/DroidTamvan/droidtamvan.github.io/src/css/bootstrap.css',
+  'https://cdn.jsdelivr.net/gh/DroidTamvan/droidtamvan.github.io/src/js/bootstrap.js'
 ];
 
 self.addEventListener('install', function (event) {
