83d87b0c8a48a07a117c15649b0a2a205f030708
DroidTamvan <ragilsuryapratama@gmail.com>
2020-02-02 07:16:56 +0700

    Update

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-var cacheNames = 'cache-34esdf';
+var cacheNames = 'cache-awdw3w';
 var fileCache = [
   './favicon.ico',
   'https://cdn.jsdelivr.net/gh/DroidTamvan/droidtamvan.github.io/src/img/1-min.jpg',
